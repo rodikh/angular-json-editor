4be0fc4a87df90230604a6cebe92fc55c590f1fe
Rodik Hanukaev <rodikh@gmail.com>
2014-06-04 22:32:12 +0300

    fixed indents

--- demo/app.js
+++ demo/app.js
@@ -2,34 +2,34 @@
 
 angular.module('demoApp', ['angular-json-editor'])
 .controller('MyCtrl', function ($scope) {
-	$scope.ctrlSchema = {
-		type: 'object',
-		properties: {
-			name: {
-				type: 'string',
-				title: 'Item Name',
-				required: true,
-				minLength: 1
-			},
-			age: {
-				type: 'integer',
-				title: 'Age',
-				required: true,
-				min: 0
-			}
-		}
-	};
+    $scope.ctrlSchema = {
+        type: 'object',
+        properties: {
+            name: {
+                type: 'string',
+                title: 'Item Name',
+                required: true,
+                minLength: 1
+            },
+            age: {
+                type: 'integer',
+                title: 'Age',
+                required: true,
+                min: 0
+            }
+        }
+    };
 
-	$scope.startval = {
-		age: 20
-	};
+    $scope.startval = {
+        age: 20
+    };
 
-	$scope.onSubmit = function () {
-		var formData = $scope.editor.getValue();
-		console.log('formData', formData);
-	};
+    $scope.onSubmit = function () {
+        var formData = $scope.editor.getValue();
+        console.log('formData', formData);
+    };
 });
 
 angular.element(window.document).ready(function () {
-    angular.bootstrap(window.document, ['demoApp']);
+	angular.bootstrap(window.document, ['demoApp']);
 });
--- src/angular-json-editor.js
+++ src/angular-json-editor.js
@@ -5,52 +5,52 @@ angular.module('angular-json-editor', [])
     return {
         restrict: 'A',
         scope: {
-			startval: '=',
-			jsonEditor: '=',
-			schemaUrl: '@',
-			editor: '=',
-			isValid: '='
-		},
+            startval: '=',
+            jsonEditor: '=',
+            schemaUrl: '@',
+            editor: '=',
+            isValid: '='
+        },
         link: function (scope, element, attrs) {
-			var config = {
-				startval: scope.startval,
-				iconlib: attrs.iconlib || 'bootstrap3',
-				theme: attrs.theme || 'bootstrap3'
-			};
+            var config = {
+                startval: scope.startval,
+                iconlib: attrs.iconlib || 'bootstrap3',
+                theme: attrs.theme || 'bootstrap3'
+            };
 
-			if (scope.jsonEditor) {
-				config.schema = scope.jsonEditor;
+            if (scope.jsonEditor) {
+                config.schema = scope.jsonEditor;
 
-			} else if (attrs.schemaUrl) {
-				config.ajax = true;
-				config.schema = {
-					$ref: scope.schemaUrl
-				};
+            } else if (attrs.schemaUrl) {
+                config.ajax = true;
+                config.schema = {
+                    $ref: scope.schemaUrl
+                };
 
-			} else {
-				console.error('no schema specified.');
-			}
+            } else {
+                console.error('no schema specified.');
+            }
 
-			var editor = new JSONEditor(element[0], config);
+            var editor = new JSONEditor(element[0], config);
 
-			// Attach the editor object to the parent scope, for easy access from a controller.
-			if (attrs.editor) {
-				scope.editor = editor;
-			}
+            // Attach the editor object to the parent scope, for easy access from a controller.
+            if (attrs.editor) {
+                scope.editor = editor;
+            }
 
-			// scope.isValid holds the validation state of the entire form.
-			// it is useful for disabling the submit button while the form isn't valid.
-			if (attrs.isValid) {
-				scope.isValid = false;
-				editor.on('ready', function () {
-					scope.isValid = (editor.validate().length === 0);
-					editor.on('change', function () {
-						scope.$apply(function () {
-							scope.isValid = (editor.validate().length === 0);
-						});
-					});
-				});
-			}
+            // scope.isValid holds the validation state of the entire form.
+            // it is useful for disabling the submit button while the form isn't valid.
+            if (attrs.isValid) {
+                scope.isValid = false;
+                editor.on('ready', function () {
+                    scope.isValid = (editor.validate().length === 0);
+                    editor.on('change', function () {
+                        scope.$apply(function () {
+                            scope.isValid = (editor.validate().length === 0);
+                        });
+                    });
+                });
+            }
         }
     };
 });
