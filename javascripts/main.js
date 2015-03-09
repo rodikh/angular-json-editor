caea8242a4abb04260f186a3fe5d47364be1b7bc
Rodik Hanukaev <rodik@chegg.com>
2015-03-09 14:35:41 +0200

    removed unneeded files added readme to page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,43 @@
-console.log('This would be the main JS file.');
+'use strict';
+
+angular.module('demoApp', ['angular-json-editor']).config(function (JSONEditorProvider) {
+    // these are set by default, but we set this for demonstration purposes
+    JSONEditorProvider.configure({
+        defaults: {
+            options: {
+                iconlib: 'bootstrap3',
+                theme: 'bootstrap3'
+            }
+        }
+    });
+
+}).controller('SyncAppController', function ($scope) {
+
+    $scope.mySchema = {
+        type: 'object',
+        properties: {
+            name: {
+                type: 'string',
+                title: 'Item Name',
+                required: true,
+                minLength: 1
+            },
+            age: {
+                type: 'integer',
+                title: 'Age',
+                required: true,
+                min: 0
+            }
+        }
+    };
+
+    $scope.myStartVal = {
+        age: 20
+    };
+
+    $scope.onChange = function (data) {
+        console.log('Form changed!');
+        console.dir(data);
+    };
+
+});
